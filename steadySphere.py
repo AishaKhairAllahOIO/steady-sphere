a8f62dff4f74b9e4753893c3404838ce36da2c0f
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-14 05:56:46 +0300

    fix method getRangeHSV()  argument by removing unnecessary 'self' parameter

--- steadySphere.py
+++ steadySphere.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-def getRangeHSV(self,BGR_color):
+def getRangeHSV(BGR_color):
         BGR_color=np.uint8([[BGR_color]])
         HSV_color=cv.cvtColor(BGR_color,cv.COLOR_BGR2HSV)
 
