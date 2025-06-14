0d077d28016be27a4f8e91f3fb2721d5f89609ab
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-14 05:43:28 +0300

    calculate lower and upper HSV bound

--- steadySphere.py
+++ steadySphere.py
@@ -4,3 +4,6 @@ import numpy as np
 def getRangeHSV(self,BGR_color):
         BGR_color=np.uint8([[BGR_color]])
         HSV_color=cv.cvtColor(BGR_color,cv.COLOR_BGR2HSV)
+
+        lower_color=HSV_color[0][0][0]-10,100,100
+        upper_color=HSV_color[0][0][0]+10,255,255
