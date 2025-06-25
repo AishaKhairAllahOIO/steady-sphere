601e714afaed861fce7fa2fa8dd140fb16193c15
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-25 17:42:47 +0300

    Add basic module for maze game

---
+++ mazeGame.py
@@ -0,0 +1,21 @@
+class Player:
+
+    def __init__(self,name:str):
+        ...
+
+    def playMethod(self):
+        pass
+
+    def getDuration():
+        ...
+
+class HumanPlayer(Player):
+    ...
+    def playMethod(self):
+        ...
+
+class RobotPlayer(Player):
+    ...
+    def playMethod(self):
+        ...
+  
