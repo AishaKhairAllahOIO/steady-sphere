930005d97ad691afb8a981b0082596186210b739
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-27 13:50:33 +0300

    Define abstract Player class with timer logic

--- mazeGame.py
+++ mazeGame.py
@@ -1,13 +1,28 @@
-class Player:
+import time
+from abc import ABC,abstractmethod
 
+class Player(ABC):
     def __init__(self,name:str):
-        ...
+        self.name=name
+        self.start_time=None
+        self.end_time=None
 
+    def start_timer(self):
+        self.start_time=time.time()
+
+    def stop_timer(self):
+        self.end_time=time.time()
+
+    def get_duration(self):
+        if self.start_time is not None and self.end_time is not None:
+            return self.end_time-self.start_time
+        else:
+            return None
+        
+    @abstractmethod
     def playMethod(self):
-        pass
+        pass    
 
-    def getDuration():
-        ...
 
 class HumanPlayer(Player):
     ...
