0aafcaf13d5ee7112c6b7d70f6fb4e0c1f242ed2
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-08-26 10:23:12 +0300

    some modify

--- steadySphere.py
+++ steadySphere.py
@@ -1,63 +1,32 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 import numpy as np
+from playsound import playsound
 # import serial
 import time
 import threading
-from playsound import playsound
-
-class LogColor:
-    RESET   = "\033[0m"
-    GREEN   = "\033[92m"
-    YELLOW  = "\033[93m"
-    BLUE    = "\033[94m"
-    PURPLE  = "\033[95m"
-    GRAY    = "\033[90m"
-    PINK    = "\033[38;5;213m" 
-    PINK_M   ="\033[95m" 
-
-def log_msg(message,level="INFO"):
-    colors={
-        "SUCCESS": LogColor.PINK_M,     
-        "INFO": LogColor.PINK,     
-        "ERROR": LogColor.PURPLE,     
-        "CRITICAL": LogColor.BLUE,  
-        "PLATFORM":LogColor.GREEN, 
-        "BALL": LogColor.YELLOW,    
-      
-    }
-    prefixes = {
-        "SUCCESS": "[SUCCESS]",
-        "INFO": "[INFO]",
-        "ERROR": "[ERROR]",
-        "CRITICAL": "[MAZE]",
-        "PLATFORM": "[PLATFORM]",
-        "BALL": "[BALL]",
-    }
-    level_upper=level.upper()
-    color=colors.get(level_upper,"\033[0m")
-    pre=prefixes.get(level_upper,f"[{level_upper}]")
-    reset="\033[0m"
-    print(f"{color}{pre}: {message}{reset}")
-
 
 
 class SoundManager:
+
     def __init__(self,sound_path):
-        self.sound_played=False
+        self.has_played=False
         self.sound_path=sound_path
 
     def play_sound(self):
         try:
             playsound(self.sound_path)
         except Exception as e:
-            log_msg(f"Sound play error: {e}", "ERROR")
+            print("Sound play error")
+
     def try_play(self):
-        if not self.sound_played:
+        if not self.has_played:
             threading.Thread(target=self.play_sound,daemon=True).start()
-            self.sound_played=True
+            self.has_played=True
 
 
 class ColorTracker:
+
     def __init__(self):
         pass
 
@@ -170,75 +139,64 @@ class ColorTracker:
 
         return rect
     
-    def find_maze_blue(self,frame,HSV_frame,lower_color, upper_color):
+    def find_maze_blue(self,frame,HSV_frame,lower_color,upper_color):
         mask_clean,contours=self.prepareFrameForContour(HSV_frame,lower_color,upper_color,cv.MORPH_RECT)
 
         contours, _=cv.findContours(mask_clean,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
         if not contours:
             return None,None,None
-
         best_contour=max(contours,key=cv.contourArea)
         area=cv.contourArea(best_contour)
         if area<1000:
             return None,None,None
-
         epsilon=0.02*cv.arcLength(best_contour,True)
         approx=cv.approxPolyDP(best_contour,epsilon,True)
-
         if len(approx)!=4:
             return None,None,None
-
-        points=approx.reshape(4, 2)
+        points=approx.reshape(4,2)
         rect=self.order_points(points)
-
-        width = int(max(np.linalg.norm(rect[0] - rect[1]), np.linalg.norm(rect[2] - rect[3])))
-        height = int(max(np.linalg.norm(rect[0] - rect[3]), np.linalg.norm(rect[1] - rect[2])))
-
-        dst = np.array([
-            [0, 0],
-            [width - 1, 0],
-            [width - 1, height - 1],
-            [0, height - 1]
-        ], dtype="float32")
-
+        width=int(max(np.linalg.norm(rect[0]-rect[1]),np.linalg.norm(rect[2]-rect[3])))
+        height=int(max(np.linalg.norm(rect[0]-rect[3]),np.linalg.norm(rect[1]-rect[2])))
+        dst=np.array([
+            [0,0],
+            [width-1,0],
+            [width-1,height-1],
+            [0,height-1]
+        ],dtype="float32")
         M=cv.getPerspectiveTransform(rect,dst)
         warped=cv.warpPerspective(frame,M,(width,height))
 
         return warped,rect,mask_clean
     
-    def maze_to_matrix(self,maze_img,grid_size):
-        hsv = cv.cvtColor(maze_img, cv.COLOR_BGR2HSV)
+    def maze_to_matrix(self,maze_img,grid_size,wall_lower_color,wall_upper_color,path_lower_color,path_upper_color):
+        HSV_frame= cv.cvtColor(maze_img, cv.COLOR_BGR2HSV)
 
-        lower_black = np.array([0, 0, 0])
-        upper_black = np.array([180, 255, 50])
-        mask_black = cv.inRange(hsv, lower_black, upper_black)
+        # lower_black = np.array([0, 0, 0])
+        # upper_black = np.array([180, 255, 50])
+        # lower_green = np.array([35, 40, 40])
+        # upper_green = np.array([85, 255, 255])
+        mask_black = cv.inRange(HSV_frame,wall_lower_color,wall_upper_color)
+        mask_green = cv.inRange(HSV_frame,path_lower_color,path_upper_color)
 
-        lower_green = np.array([35, 40, 40])
-        upper_green = np.array([85, 255, 255])
-        mask_green = cv.inRange(hsv, lower_green, upper_green)
-
-        h, w = maze_img.shape[:2]
-        rows, cols = grid_size
-        cell_h, cell_w = h // rows, w // cols
-
-        maze_matrix = np.zeros((rows, cols), dtype=np.int32)
+        h,w=maze_img.shape[:2]
+        rows,cols=grid_size
+        cell_h,cell_w=h//rows,w//cols
+        maze_matrix=np.zeros((rows,cols),dtype=np.int32)
 
         for i in range(rows):
             for j in range(cols):
-                y1, y2 = i*cell_h, (i+1)*cell_h
-                x1, x2 = j*cell_w, (j+1)*cell_w
-
-                cell_black = np.sum(mask_black[y1:y2, x1:x2]) / 255
-                cell_green = np.sum(mask_green[y1:y2, x1:x2]) / 255
-
-                maze_matrix[i, j] = 1 if cell_black > cell_green else 0
+                y1,y2=i*cell_h,(i+1)*cell_h
+                x1,x2=j*cell_w,(j+1)*cell_w
+                cell_black=np.sum(mask_black[y1:y2,x1:x2])/255
+                cell_green=np.sum(mask_green[y1:y2,x1:x2])/255
+                maze_matrix[i,j]=1 if cell_black>cell_green else 0
 
         return maze_matrix
     
     @staticmethod
-    def solve_maze(maze_matrix, start=(0,0), end=None):
+    def solve_maze(maze_matrix,start,end):
         from queue import Queue
-        rows, cols = maze_matrix.shape
+        rows,cols=maze_matrix.shape
         if end is None:
             end = (rows-1, cols-1)
 
@@ -315,28 +273,18 @@ class GimbalController:
 # time.sleep(2)    
 
 yellow=[0,255,255]
-green=[192,142,255]
+center=[192,142,255]
 blue=[177,118,18]
-
-
 colorTracker=ColorTracker()
 lower_yellow,upper_yellow=colorTracker.getRangeHSV(yellow)
-lower_green,upper_green=colorTracker.getRangeHSV(green)
+lower_center,upper_center=colorTracker.getRangeHSV(center)
 lower_blue,upper_blue=colorTracker.getRangeHSV(blue)
+wall_lower_color=np.array([0,0,0])
+wall_upper_color=np.array([180,255,50])
+path_lower_color=np.array([35,40,40])
+path_upper_color=np.array([85,255,255])
 
-log_msg("Color use for track in HSV:","INFO")
-log_msg("Yellow color HSV range:","BALL")
-log_msg(f"Lower Yellow: {lower_yellow}","BALL")
-log_msg(f"Upper Yellow: {upper_yellow}","BALL")
-log_msg("Green color HSV range:","PLATFORM")
-log_msg(f"Lower Green: {lower_green}","PLATFORM")
-log_msg(f"Upper Green: {upper_green}","PLATFORM")
-log_msg("Blue color HSV range:","MAZE")
-log_msg(f"Lower Blue: {lower_blue}","MAZE")
-log_msg(f"Upper Blue: {upper_blue}","MAZE")
-print("\n")
-
-videoCapture=cv.VideoCapture(1,cv.CAP_DSHOW)
+videoCapture=cv.VideoCapture(0,cv.CAP_DSHOW)
 videoCapture.set(cv.CAP_PROP_FRAME_WIDTH,640)
 videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT,480)
 
@@ -363,9 +311,8 @@ while True:
     frame,HSV_frame=colorTracker.prepareFrameForTrackingByHSV(frame)
     maze_img,maze_rect,maze_mask=colorTracker.find_maze_blue(frame,HSV_frame,lower_blue,upper_blue)
 
-
     if not platform_center_locked:
-        mask_clean,platform_X,platform_Y=colorTracker.track_rectangle_position(frame,HSV_frame,lower_green,upper_green)
+        mask_clean,platform_X,platform_Y=colorTracker.track_rectangle_position(frame,HSV_frame,lower_center,upper_center)
         if platform_X is not None and platform_Y is not None:
             saved_platform_X=platform_X
             saved_platform_Y=platform_Y
@@ -374,14 +321,14 @@ while True:
 
     if key==ord('l'):
         platform_center_locked=True
-        log_msg("Platform tracking locked","INFO")
+        print("Platform tracking locked")
     elif key==ord('o'):
         platform_center_locked=False
-        log_msg("Platform tracking unlocked","INFO")
-    log_msg(f"Platform  center: X={saved_platform_X}, Y={saved_platform_Y}", "PLATFORM")
+        print("Platform tracking unlocked","INFO")
+    print(f"Platform  center: X={saved_platform_X}, Y={saved_platform_Y}", "PLATFORM")
     
     mask_clean,ballCenter_X,ballCenter_Y=colorTracker.track_ball_position(frame,HSV_frame,lower_yellow,upper_yellow)
-    log_msg(f"Ball  center: X={ballCenter_X}, Y={ballCenter_Y}", "BALL")
+    print(f"Ball  center: X={ballCenter_X}, Y={ballCenter_Y}", "BALL")
     print("\n")
 
     if ballCenter_X is not None and ballCenter_Y is not None: 
@@ -389,30 +336,30 @@ while True:
 
     if ballCenter_X is not None and saved_platform_X is not None:
         error_x=saved_platform_X-ballCenter_X 
-        log_msg(f"Error X={error_x}", "ERROR")
+        print(f"Error X={error_x}", "ERROR")
         print("\n")
         if abs(error_x)<5:
             output_x=0
-            log_msg("X Dead Zone-No correction needed.","INFO")
+            print("X Dead Zone-No correction needed.","INFO")
         else:
             output_x=pid_x.PIDcompute(error_x)
-            log_msg(f"pid x={output_x}", "INFO")
+            print(f"pid x={output_x}", "INFO")
 
         angle_x=gimbalController_x.mapPIDtoAngle(output_x,-100,100,125,155)
-        log_msg(f"angle x={angle_x}", "INFO")
+        print(f"angle x={angle_x}", "INFO")
 
     if ballCenter_Y is not None and saved_platform_Y is not None:
         error_y=saved_platform_Y-ballCenter_Y
-        log_msg(f"Error Y={error_y}", "ERROR")
+        print(f"Error Y={error_y}", "ERROR")
         if abs(error_y) <5:
             output_y = 0
-            log_msg("Y Dead Zone-No correction needed.","INFO")
+            print("Y Dead Zone-No correction needed.","INFO")
         else:
             output_y = pid_y.PIDcompute(error_y)
-            log_msg(f"pid y={output_y}", "INFO")
+            print(f"pid y={output_y}", "INFO")
 
         angle_y=gimbalController_y.mapPIDtoAngle(output_y,-100,100,140,180)
-        log_msg(f"angle y={angle_y}", "INFO")
+        print(f"angle y={angle_y}", "INFO")
 
 
     # if 'angle_x' in locals() and 'angle_y' in locals():
@@ -491,11 +438,11 @@ while True:
         maze_img,maze_rect,_=colorTracker.find_maze_blue(frame,HSV_frame,lower_blue,upper_blue)
         if maze_img is not None:
             cv.imshow("Cropped Maze", maze_img)
-            log_msg("Maze cropped!", "CRITICAL")
+            print("Maze cropped!", "CRITICAL")
             save_path = "cropped_maze.png" 
             cv.imwrite(save_path, maze_img)
-            log_msg(f"Maze saved to {save_path}", "INFO")
-            maze_matrix = colorTracker.maze_to_matrix(maze_img, grid_size=(50,50))
+            print(f"Maze saved to {save_path}", "INFO")
+            maze_matrix = colorTracker.maze_to_matrix(maze_img, grid_size=(40,40))
             maze_solved, path = colorTracker.solve_maze(maze_matrix)
            
             maze_sol="maze_solution.txt"
@@ -511,7 +458,7 @@ while True:
                     f.write(row_str + "\n")
 
         else:
-            log_msg("No valid maze found to crop!", "ERROR")
+            print("No valid maze found to crop!", "ERROR")
     
 
 videoCapture.release()
